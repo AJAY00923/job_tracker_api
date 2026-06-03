6abe1cabd4ea2180faff37a7bbf49a03a2dd0c3f
Ajay Gopavarapu <ajaygopavrapu143@gmail.com>
2026-06-03 16:35:11 -0500

    feat: Full CRUD complete - Post, Get,Put, Delete routes Working fine

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from schemas import JobApplicationCreate, JobApplicationUpdate
+from typing import List
 from fastapi import FastAPI
-from schemas import JobApplicationCreate
 
 app = FastAPI()
 
@@ -8,11 +9,29 @@ applications = []
 def read_root():
     return {"message":"Job Tracker Api is running!"}
 
-@app.post("/applications")
-def create_application(application: JobApplicationCreate):
+@app.get("/applications", response_model = dict)
+def get_applications() -> dict:
+    return {"applications": applications}
+
+@app.post("/applications", status_code=201)
+def create_application(application: JobApplicationCreate) -> dict:
     applications.append(application)
     return {"message": "Application added successfully", "application": application}
 
-@app.get("/applications")
-def get_applications():
-    return {"applications": applications}
+@app.put("/applications/{company}")
+def update_application(company: str, update: JobApplicationUpdate) -> dict:
+    for application in applications:
+        if application.company == company:
+            application.status = update.status
+            return {"message": f"Application for {company} updated to '{update.status}'."}  
+
+    return {'message' : f"NO Application found for  '{company}'."}
+
+@app.delete("/applications/{company}")
+def delete_application(company: str) -> dict:
+    global applications
+    for application in applications:
+        if application.company == company:
+            applications.remove(application)
+            return {"message": f"Application for {company} deleted successfully."}
+    return {"message": f"No application found for {company}."}  
--- schemas.py
+++ schemas.py
@@ -5,4 +5,7 @@ class JobApplicationCreate(BaseModel):
     company : str
     role : str
     status : Optional[str] = "applied"
+
+class JobApplicationUpdate(BaseModel):
+    status : str
     
